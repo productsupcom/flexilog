640d7136bf5caaa95281960dd39f8df338c429f6
Chris Sachs <cs@productsup.com>
2015-07-23 14:41:51 +0200

    add a function to create the logger from a string

--- lib/Productsup/LogInfo.php
+++ lib/Productsup/LogInfo.php
@@ -22,4 +22,21 @@ class LogInfo
      * @var string $facility the Facility that is being Logged from
      */
     public $facility = null;
+
+    /**
+     * @param string $string a string like site:123;process:abcdef;facility:destination
+     * @return static
+     */
+    public static function fromString($string) {
+        $parts = explode(';',$string);
+        $info = new static;
+        foreach($parts as $part) {
+
+            $property = explode(':',$part,2);
+            if(property_exists($info,$property[0])) {
+                $info->{$property[0]} = $property[1];
+            }
+        }
+        return $info;
+    }
 }

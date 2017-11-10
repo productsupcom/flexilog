4b019424a75620b817fba2d1edfca60bc4d82529
Werner Spiegel <ws@products-up.de>
2017-11-10 12:28:00 +0100

    use the handlerexception

--- lib/Handler/FileHandler.php
+++ lib/Handler/FileHandler.php
@@ -71,7 +71,7 @@ class FileHandler extends AbstractHandler
     public function writeToFile($line)
     {
         if (file_put_contents($this->filename, $line, FILE_APPEND | LOCK_EX) === false) {
-            throw new HandlerException('Cannot write to file: '.$this->handle);
+            throw new HandlerException('Cannot write to file: ' . $this->filename);
         }
     }
 }
